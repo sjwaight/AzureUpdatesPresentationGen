b4dfe0f16bccf3d41e6ecba05cb68cb24fdb71dd
Simon Waight <simon.waight@microsoft.com>
2025-03-24 14:56:17 +1100

    Convert to use OData API

--- GeneratePresentation/__init__.py
+++ GeneratePresentation/__init__.py
@@ -20,33 +20,36 @@ def get_updates_rss(startDate, endDate):
         # execute my request, parse the data using XML
         # parse using BS4
         r = requests.get(os.environ["UpdatesURL"])
-        soup = BeautifulSoup(r.content, features='xml')
+        # soup = BeautifulSoup(r.content, features='xml')
+        updatesData = r.json()
 
         # select only the "items" I want from the data
-        updates = soup.findAll('item')
+        #updates = soup.findAll('item')
+        updates = updatesData.get("value", [])
 
         # for each "item" I want, parse it into a list
         for a in updates:
 
             # Get publication date
-            published = a.find('pubDate').text
-            pubDate = datetime.strptime(a.find('pubDate').text, "%a, %d %b %Y %H:%M:%S Z")
+            published = a.get("modified")
+            pubDate = datetime.strptime(published, "%a, %d %b %Y %H:%M:%S Z")
 
             # only include items falling within our requested date range
             if (pubDate >= startDate and pubDate <= endDate):
 
-                title = a.find('title').text
-                link = a.find('link').text
+                title = a.get('title')
+                link = 'https://azure.microsoft.com/en-us/updates/?id=' + a.get('id') 
                 # Extract categories as an array
-                categories = [category.text for category in a.findAll('category')]
 
-                # basic parse to flag announcement types
-                if "Retirement" in categories:
-                    announcement_type = "retirement"
-                elif "In preview" in categories:
-                     announcement_type = "preview"
-                elif "Launched" in categories:
-                    announcement_type = "GA"
+
+                category = a["availabilities"][0]["ring"]
+
+                # Map categories to announcement types
+                category_to_announcement_type = {
+                    "Retirement": "retirement",
+                    "Preview": "preview",
+                    "General Availability": "GA"
+                }
 
                 # create an "article" object with the data
                 # from each "item"
@@ -54,7 +57,7 @@ def get_updates_rss(startDate, endDate):
                     'title': title,
                     'link': link,
                     'published': published,
-                    'antype': announcement_type
+                    'antype': category_to_announcement_type.get(category, "unknown")
                     }
 
                 # append my "article_list" with each "article" object
@@ -63,7 +66,7 @@ def get_updates_rss(startDate, endDate):
         # after the loop, dump my saved objects into a .txt file
         return article_list
     except Exception as e:
-        logging.exception("Couldn't scrape the Azure Updates RSS feed")
+        logging.exception("Couldn't scrape the Azure Updates OData feed")
 
 ###
 # Generate a section of the final PowerPoint
